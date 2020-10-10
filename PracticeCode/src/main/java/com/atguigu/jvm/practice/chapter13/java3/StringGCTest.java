116817d0200f7455ab41d8e1b13306912c407e99
cococolabear <794055465@qq.com>
2020-10-10 21:48:27 +0800

    add

--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter13/java3/StringGCTest.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter13/java3/StringGCTest.java
@@ -6,4 +6,9 @@ package com.atguigu.jvm.practice.chapter13.java3;
  * @date 2020/10/7 3:33 下午
  */
 public class StringGCTest {
+    public static void main(String[] args) {
+        for (int j = 0; j < 100000; j++) {
+            String.valueOf(j).intern();
+        }
+    }
 }
