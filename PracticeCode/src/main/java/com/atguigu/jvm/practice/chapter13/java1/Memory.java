7ea33fb5c38124aab57e68715a91ab0dc71f385b
cococolabear <794055465@qq.com>
2020-10-10 21:46:34 +0800

    add

--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter13/java1/Memory.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter13/java1/Memory.java
@@ -6,4 +6,15 @@ package com.atguigu.jvm.practice.chapter13.java1;
  * @date 2020/10/7 3:30 下午
  */
 public class Memory {
+    public static void main(String[] args) {
+        int i = 1;
+        Object obj = new Object();
+        Memory mem = new Memory();
+        mem.foo(obj);
+    }
+
+    private void foo(Object param) {
+        String str = param.toString();
+        System.out.println(str);
+    }
 }
--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter13/java1/StringTest4.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter13/java1/StringTest4.java
@@ -6,4 +6,28 @@ package com.atguigu.jvm.practice.chapter13.java1;
  * @date 2020/10/7 3:30 下午
  */
 public class StringTest4 {
+    public static void main(String[] args) {
+        System.out.println();//2293
+        System.out.println("1");//2294
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+        System.out.println("9");
+        System.out.println("10");//2303
+        //如下的字符串"1" 到 "10"不会再次加载
+        System.out.println("1");//2304
+        System.out.println("2");//2304
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+        System.out.println("9");
+        System.out.println("10");//2304
+    }
 }
