4e4f8021ed1ef3ebe2cf1f25196ad88a9e002291
cococolabear <794055465@qq.com>
2020-10-11 12:11:39 +0800

    add

--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter15/java/GCRootsTest.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter15/java/GCRootsTest.java
@@ -1,9 +1,38 @@
 package com.atguigu.jvm.practice.chapter15.java;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
  * @date 2020/10/7 3:42 下午
  */
 public class GCRootsTest {
+    public static void main(String[] args) {
+        List<Object> numList = new ArrayList<>();
+        Date birth = new Date();
+
+        for (int i = 0; i < 100; i++) {
+            numList.add(String.valueOf(i));
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("数据添加完毕，请操作：");
+        new Scanner(System.in).next();
+        numList = null;
+        birth = null;
+
+        System.out.println("numList、birth已置空，请操作：");
+        new Scanner(System.in).next();
+
+        System.out.println("结束");
+    }
+
 }
--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter15/java/HeapOOM.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter15/java/HeapOOM.java
@@ -1,9 +1,28 @@
 package com.atguigu.jvm.practice.chapter15.java;
 
+import java.util.ArrayList;
+
 /**
  * @author Duan Xiangqing
  * @version 1.0
  * @date 2020/10/7 3:42 下午
  */
 public class HeapOOM {
+    byte[] buffer = new byte[1 * 1024 * 1024];//1MB
+
+    public static void main(String[] args) {
+        ArrayList<HeapOOM> list = new ArrayList<>();
+
+        int count = 0;
+
+        try {
+            while (true) {
+                list.add(new HeapOOM());
+                count++;
+            }
+        } catch (Throwable e) {
+            System.out.println("count = " + count);
+            e.printStackTrace();
+        }
+    }
 }
