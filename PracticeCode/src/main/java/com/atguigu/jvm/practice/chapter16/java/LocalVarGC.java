a667dd2d9f958bbc27a085f0c23950d2a1448f2b
cococolabear <794055465@qq.com>
2020-10-11 23:56:26 +0800

    add

--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter16/java/LocalVarGC.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter16/java/LocalVarGC.java
@@ -6,4 +6,17 @@ package com.atguigu.jvm.practice.chapter16.java;
  * @date 2020/10/7 3:44 下午
  */
 public class LocalVarGC {
+    public void localvarGC1(){
+        byte[] buffer=new byte[10*1024*1024];//10MB
+        System.gc();
+    }
+
+    public void localvarGC2(){
+        byte[] buffer = new byte[10 * 1024 * 1024];
+        buffer = null;
+        System.gc();
+
+    }
+
+    public 
 }
