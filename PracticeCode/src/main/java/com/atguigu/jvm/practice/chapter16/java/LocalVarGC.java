5f678406dc53c4804bb6d0d62ccf3370f744be01
cococolabear <794055465@qq.com>
2020-10-13 01:04:49 +0800

    add

--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter16/java/LocalVarGC.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter16/java/LocalVarGC.java
@@ -6,17 +6,40 @@ package com.atguigu.jvm.practice.chapter16.java;
  * @date 2020/10/7 3:44 下午
  */
 public class LocalVarGC {
-    public void localvarGC1(){
-        byte[] buffer=new byte[10*1024*1024];//10MB
+    public void localvarGC1() {
+        byte[] buffer = new byte[10 * 1024 * 1024];//10MB
         System.gc();
     }
 
-    public void localvarGC2(){
+    public void localvarGC2() {
         byte[] buffer = new byte[10 * 1024 * 1024];
         buffer = null;
         System.gc();
-
     }
 
-    public 
+    public void localvarGC3() {
+        {
+            byte[] buffer = new byte[10 * 1024 * 1024];
+        }
+        System.gc();
+    }
+
+    public void localvarGC4() {
+        {
+            byte[] buffer = new byte[10 * 1024 * 1024];
+        }
+        int value = 10;
+        System.gc();
+    }
+
+    public void localvarGC5() {
+        localvarGC1();
+        System.gc();
+    }
+
+    public static void main(String[] args) {
+        LocalVarGC local = new LocalVarGC();
+        local.localvarGC5();
+    }
 }
+
--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter16/java/SystemGCTest.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter16/java/SystemGCTest.java
@@ -6,4 +6,17 @@ package com.atguigu.jvm.practice.chapter16.java;
  * @date 2020/10/7 3:45 下午
  */
 public class SystemGCTest {
+    public static void main(String[] args) {
+        new SystemGCTest();
+        System.gc();//提醒jvm的垃圾回收器执行gc,但是不确定是否马上执行gc
+        //与Runtime.getRuntime().gc();的作用一样。
+
+        System.runFinalization();//强制调用使用引用的对象的finalize()方法
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("SystemGCTest 重写了finalize()");
+    }
 }
