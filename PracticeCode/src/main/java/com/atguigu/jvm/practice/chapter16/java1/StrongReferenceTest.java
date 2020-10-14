ca3b3de101cd260a68464986c37a47d22180c03a
cococolabear <794055465@qq.com>
2020-10-14 15:33:02 +0800

    add

--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter16/java1/StrongReferenceTest.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter16/java1/StrongReferenceTest.java
@@ -7,8 +7,19 @@ package com.atguigu.jvm.practice.chapter16.java1;
  */
 public class StrongReferenceTest {
     public static void main(String[] args) {
+        StringBuffer str = new StringBuffer("hello");
+        StringBuffer str1 = str;
 
+        str = null;
+        System.gc();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(str1);
     }
-
 }
 
