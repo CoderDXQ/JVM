c589a56250f8dd5c2872ed1a449f26f32c4ed004
cococolabear <794055465@qq.com>
2020-10-19 18:01:14 +0800

    add

--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter02/lang/ShkStart.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter02/lang/ShkStart.java
@@ -5,8 +5,8 @@ package com.atguigu.jvm.practice.chapter02.lang;
  * @version 1.0
  * @date 2020/10/8 12:26 上午
  */
-public class ShkStart {
-    public static void main(String[] args) {//因为lang与jdk的lang重名 所以无法进行加载
+public class ShkStart {//下面的String要用原来JDK里的
+    public static void main(java.lang.String[] args) {//因为lang与jdk的lang重名 所以无法进行加载
         System.out.println("hello!");
     }
 }
--- PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter02/lang/String.java
+++ PracticeCode/src/main/java/com/atguigu/jvm/practice/chapter02/lang/String.java
@@ -10,8 +10,10 @@ public class String {
     static {
         System.out.println("我是自定义的String类的静态代码块");
     }
-    //错误：在类java.lang.String中找不到main方法
-    public static void main(String[] args) {
+
+    //错误：在类java.lang.String中找不到main方法 //下面的String要用原来JDK里的
+    public static void main(java.lang.String[] args) {
+        String st;//实例化的时候会进入静态代码块 然后输出
         System.out.println("hello，String");
     }
 }
